dec65b8fcc5d0401a19be47c9fd9f27d69aa8cd4
Wiesen <yangwj92@zju.edu.cn>
2016-06-19 08:49:16 +0800

    update

--- javascripts/getmd.js
+++ javascripts/getmd.js
@@ -1,4 +1,4 @@
-var showdown  = require('showdown'),
+var showdown  = require('showdown');
 var path = decodeURIComponent(location.hash.substr(2));
 var page = 1;
 if(location.search.substr(1,19)=='_escaped_fragment_='){
